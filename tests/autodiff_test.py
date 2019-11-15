d436c215e17d99e22686a3c69a7eb9e21b49ff1c
zdd <zdd@dhcp-10-250-172-45.harvard.edu>
2019-11-14 22:52:40 -0500

    fixing a typo

--- tests/autodiff_test.py
+++ tests/autodiff_test.py
@@ -4,7 +4,7 @@ from jeeautodiff.autodiff import *
 def test_add():
     x = Node(2.0) + Node(1.0)
     assert(x.val==3)
-    aseert(x.der==2)
+    assert(x.der==2)
 
 def test_equal():
     x=Node(2.0) + Node(1.0)
