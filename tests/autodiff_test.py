c029f37fa527247a2aaec7ad8f968b3fad6233ce
evan_simons <evan.r.simons@gmail.com>
2019-11-19 15:12:29 -0500

    Added tests for Node class and revised some syntax

--- tests/autodiff_test.py
+++ tests/autodiff_test.py
@@ -1,21 +1,88 @@
 import pytest
 import jeeautodiff as ad
 
+
 def test_add():
     x = ad.Node(2.0) + ad.Node(1.0)
-    assert(x.val==3)
-    assert(x.der==2)
+    assert x.val == 3
+    assert x.der == 2
+
 
 def test_add_num():
     x = ad.Node(2.0) + 5
-    assert(x.val==7)
-    assert(x.der==1)
+    assert x.val == 7
+    assert x.der == 1
+
+
+def test_radd():
+    x = 5 + ad.Node(2.0)
+    assert x.val == 7
+    assert x.der == 1
+
 
 def test_equal():
-    x=ad.Node(2.0) + ad.Node(1.0)
-    y=ad.Node(3.0,2.0)
-    assert (x==y)
+    x = ad.Node(2.0) + ad.Node(1.0)
+    y = ad.Node(3.0, 2.0)
+    assert x == y
 
 
+def test_sub():
+    x = ad.Node(5.0) - ad.Node(3)
+    assert x.val == 2
+    assert x.der == 0
 
 
+def test_sub_num():
+    x = ad.Node(5.0) - 3
+    assert x.val == 2
+    assert x.der == 1
+
+
+def test_rsub():
+    x = 5 - ad.Node(2.0)
+    assert x.val == 3
+    assert x.der == -1
+
+
+def test_mul():
+    x = ad.Node(5.0) * ad.Node(4)
+    assert x.val == 20
+    assert x.der == 9
+
+
+def test_mul_num():
+    x = ad.Node(5.0) * 4
+    assert x.val == 20
+    assert x.der == 4
+
+
+def test_rmul():
+    x = 5 * ad.Node(4)
+    assert x.val == 20
+    assert x.der == 5
+
+
+def test_truediv():
+    pass
+
+
+def test_truediv_num():
+    pass
+
+
+def test_rtruediv():
+    pass
+
+
+def test_pow():
+    pass
+
+
+def test_pow_num():
+    pass
+
+
+def test_neg():
+    x = -ad.Node(5.0)
+    assert x.val == 5
+    assert x.der == -1
--- tests/utility_test.py
+++ tests/utility_test.py
@@ -1,58 +1,63 @@
 import pytest
 import numpy as np
 import jeeautodiff as ad
-def test_sin():
-    x=ad.Node(1.0)
-    x=ad.sin(x)
-    assert (x.val==np.sin(1))
-    assert (x.der==np.cos(1))
 
-    x=ad.sin(np.pi)
-    assert(x==np.sin(np.pi))
+
+def test_sin():
+    x = ad.Node(1.0)
+    x = ad.sin(x)
+    assert x.val == np.sin(1)
+    assert x.der == np.cos(1)
+
+    x = ad.sin(np.pi)
+    assert x == np.sin(np.pi)
     with pytest.raises(ValueError):
         ad.sin("1")
 
-def test_cos():
-    x=ad.Node(1.0)
-    x=ad.cos(x)
-    assert (x.val==np.cos(1))
-    assert (x.der==-np.sin(1))
 
-    x=ad.cos(np.pi)
-    assert(x==np.cos(np.pi))
+def test_cos():
+    x = ad.Node(1.0)
+    x = ad.cos(x)
+    assert x.val == np.cos(1)
+    assert x.der == -np.sin(1)
+
+    x = ad.cos(np.pi)
+    assert x == np.cos(np.pi)
     with pytest.raises(ValueError):
         ad.cos("1")
 
-def test_tan():
-    x=ad.Node(1.0,2)
-    x=ad.tan(x)
-    assert (x.val==np.tan(1))
-    assert (x.der==2*1/(np.cos(1)**2))
 
-    x=ad.tan(np.pi)
-    assert(x==np.tan(np.pi))
+def test_tan():
+    x = ad.Node(1.0, 2)
+    x = ad.tan(x)
+    assert x.val == np.tan(1)
+    assert x.der == 2 * 1 / (np.cos(1) ** 2)
+
+    x = ad.tan(np.pi)
+    assert x == np.tan(np.pi)
     with pytest.raises(ValueError):
         ad.tan("1")
 
-def test_exp():
-    x=ad.Node(0,2)
-    x=ad.exp(x)
-    assert (x.val==1)
-    assert (x.der==2)
 
-    x=ad.exp(0)
-    assert(x==1)
+def test_exp():
+    x = ad.Node(0, 2)
+    x = ad.exp(x)
+    assert x.val == 1
+    assert x.der == 2
+
+    x = ad.exp(0)
+    assert x == 1
     with pytest.raises(ValueError):
         ad.exp("1")
 
-def test_log():
-    x=ad.Node(2,2)
-    x=ad.log(x)
-    assert (x.val==np.log(2))
-    assert (x.der==1)
 
-    x=ad.log(1)
-    assert(x==0)
+def test_log():
+    x = ad.Node(2, 2)
+    x = ad.log(x)
+    assert x.val == np.log(2)
+    assert x.der == 1
+
+    x = ad.log(1)
+    assert x == 0
     with pytest.raises(ValueError):
         ad.log("1")
-
