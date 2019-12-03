8a1b43a368ace437a8d201aa12b34dccd3cb0662
zdd <zdd@dhcp-10-250-172-45.harvard.edu>
2019-12-03 15:28:42 -0500

    adding vector input and vector function

--- jeeautodiff/autodiff.py
+++ jeeautodiff/autodiff.py
@@ -1,22 +1,90 @@
 # from utility import sin
+import numpy as np
 
-
-class autodiff:
+class Autodiff:
     def __init__(self, dimension=1):
         self.count = 0
         self.dimension = dimension
 
-    def create_variable(self, val, der=1):
+    def create_variable(self, val, der=None):
         if self.count >= self.dimension:
             raise Exception("Can not create more variable than pre-specified value")
-        self.count += 1
-        return Node(val, der)
+        try:
+            float(val)
+            #position of the seed
+            position=self.count
+            self.count += 1
+            if der is None:
+                #specify initial gradient
+                gradient=np.zeros([self.dimension,])
+                gradient[position]=1
+                return Node(val, gradient)
+            else:
+                gradient = np.zeros([self.dimension, ])
+                gradient[position] = der
+                return Node(val,gradient)
+        except:
+            #user specify each seed
+            if der is not None:
+                if type(val) is not list or type(der) is not list:
+                    raise ValueError("for multiple input, both Val and Der(if not specified) should be a list")
+                #check equal length of prespecified val and der
+                if len(val)!=len(der):
+                    raise ValueError("The value and derivative you specified are not the same length")
+                if self.count+len(val)>self.dimension:
+                    raise Exception("Can not create more variable than pre-specified value")
+                #record return list of Node
+                node_ls=[]
+                for i in range(0,len(val)):
+                    position = self.count
+                    self.count += 1
+                    gradient = np.zeros([self.dimension, ])
+                    gradient[position] = der[i]
+                    node_ls.append(Node(val[i], gradient))
+                return tuple(node_ls)
+            # if user did not specify seed, then default gradient should be 1
+            else:
+                if type(val) is not list:
+                    raise ValueError("for multiple input, Val should be a list")
+                if self.count + len(val) > self.dimension:
+                    raise Exception("Can not create more variable than pre-specified value")
+                # record return list of Node
+                node_ls = []
+                for i in range(0, len(val)):
+                    position = self.count
+                    self.count += 1
+                    gradient = np.zeros([self.dimension, ])
+                    gradient[position] = 1
+                    node_ls.append(Node(val[i], gradient))
+                return tuple(node_ls)
+
+
+
 
     def eval(self, N):  # evaluate
-        try:
-            return (N.val, N.der)
-        except:
-            return (N, 0)
+        if isinstance(N,Node):
+            try:
+                return (N.val, N.der)
+            except:
+                gradient = np.zeros([self.dimension, ])
+                return (N, gradient)
+        if type(N) is list:
+            return_val=[]
+            return_jacobian=np.empty([0,self.dimension])
+            for i in N:
+                if isinstance(i,Node):
+                    return_val.append(i.val)
+                    gradient=i.der.reshape(1,self.dimension)
+                    return_jacobian=np.concatenate((return_jacobian,gradient),axis=0)
+                else:
+                    return_val.append(i)
+                    gradient = np.zeros([1,self.dimension])
+                    return_jacobian = np.concatenate((return_jacobian, gradient), axis=0)
+
+            return (return_val,return_jacobian)
+        else:
+            raise Exception("Should pass a function or a vector of function into eval")
+
 
 
 class Node:
